e8106f55d826b8c6f4ed37cc8f9ace20cda58ef7
Shyam dadhaniya <shyam.dadhaniya04@gmail.com>
2022-08-03 13:59:57 +0530

    Update axios.js

--- util/axios.js
+++ util/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.URL,
+  baseURL:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000/api/"
+      : "https://main--food-delivery-application.netlify.app/api/",
 });
 
 export default axiosInstance;
