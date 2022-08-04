2212822199a357c390b8bbec8b4152451bf1c527
Shyam dadhaniya <shyam.dadhaniya04@gmail.com>
2022-08-04 18:42:51 +0530

    Update

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   swcMinify: true,
   env: {
     MONGO_URL:"mongodb+srv://foodOrderingApp:foodapp@cluster0.wms7sch.mongodb.net/pizza?retryWrites=true&w=majority",
+    ADMIN_USERNAME:"admin",
+    ADMIN_PASSWORD:"admin",
+    TOKEN:"SWdw4CV||663Z{p3|ZXtP%0k6Ejj;F"
   },
 };
 
